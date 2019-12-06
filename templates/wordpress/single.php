b9d4d318346e65dc5575dc6e2c494cafd652f7a2
Middi <richard@richardmiddleton.me>
2019-12-06 15:41:54 +0000

    single php loop

--- templates/wordpress/single.php
+++ templates/wordpress/single.php
@@ -11,44 +11,43 @@ get_header();
 ?>
 
 <main>
-	
-	<article class="article-container">
-		<div class="title">
-			<h1 class="h1-title"><?php echo get_the_title($post_id); ?></h1>
-		</div>
-		<?php if (has_post_thumbnail($post->ID)) : ?>
-			<?php $image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'single-post-thumbnail'); ?>
-			<figure class="post-featured-image" style="background: url(<?php echo $image[0]; ?>); background-size: cover; 
-        background-position: top center;">
-				
-			</figure>
-			
-		<?php endif; ?>
 
-		<?php
-			$user = wp_get_current_user();
-			
-			if ( $user ) :
+    <article class="article-container">
+        <div class="title">
+            <h1 class="h1-title"><?php echo get_the_title($post_id); ?></h1>
+        </div>
+        <?php if (has_post_thumbnail($post->ID)) : ?>
+			<?php $image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'single-post-thumbnail'); ?>
+				<figure class="post-featured-image" style="background: url(<?php echo $image[0]; ?>); background-size: cover; background-position: top center;"></figure>
+        <?php endif; ?>
+
+        <?php if ( have_posts() ) {
+			while ( have_posts() ) {
+				the_post(); 
+				$author_id = get_the_author_meta( 'ID' );
 				?>
-				<div class="article-meta">	
-					<a href="<?php echo get_author_posts_url( $user->ID); ?>">
-						<img src="<?php echo esc_url( get_avatar_url( $user->ID, ['size' => '48'] ) ); ?>" class="avatar" />
+				<div class="article-meta">
+					<a href="<?php echo esc_url( get_author_posts_url( $author_id ) ); ?>" title="<?php echo esc_attr( get_the_author() ); ?>">
+						<img src="<?php echo esc_url( get_avatar_url( $author_id, ['size' => '48'] ) ); ?>" class="avatar" />
 					</a>
+
 					<div class="article-meta-info">
 						<p class="small">
-							<a href="<?php echo get_author_posts_url( $user->ID); ?>"><?php echo ($user->display_name); ?></a></p>
+							<a href="<?php echo esc_url( get_author_posts_url( $author_id ) ); ?>"><?php the_author(); ?></a>
+						</p>
 						<p class="small">
 							<?php echo get_the_date(); ?>
-						</p>   
+						</p>
 					</div>
-					            
 				</div>
+
+				<?php the_content(); ?>
+		<?php
+			} // end while
+		} // end if
+		?> 
 		
-			<?php endif; ?>
-		<?php while (have_posts()) : the_post(); ?>
-			<?php the_content(); ?>
-		<?php endwhile; ?>
-	</article>
+    </article>
 </main>
 
 
